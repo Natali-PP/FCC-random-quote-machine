e6b95f780b1d97ae9ae7172d2985f832962668f0
C Natalí Palacio Pastor <cristinanatalip@gmail.com>
2019-11-09 01:53:28 -0300

    Updates

---
+++ precache-manifest.5b723bc66c1ba74ed966897db9d52caa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2eb0323bb5101a847af5c5909a0cf2",
+    "url": "/FCC-random-quote-machine/index.html"
+  },
+  {
+    "revision": "84ebc11d1b876b7bfbd4",
+    "url": "/FCC-random-quote-machine/static/css/main.0a3ae458.chunk.css"
+  },
+  {
+    "revision": "5d6f0b25dc9188c84528",
+    "url": "/FCC-random-quote-machine/static/js/2.3ccf852e.chunk.js"
+  },
+  {
+    "revision": "84ebc11d1b876b7bfbd4",
+    "url": "/FCC-random-quote-machine/static/js/main.35fc85d9.chunk.js"
+  },
+  {
+    "revision": "60b56450b60454a53ee6",
+    "url": "/FCC-random-quote-machine/static/js/runtime-main.0e12826a.js"
+  },
+  {
+    "revision": "284c0280fa22bb7bb244ea6765f78223",
+    "url": "/FCC-random-quote-machine/static/media/background-desktop.284c0280.svg"
+  },
+  {
+    "revision": "b0c85541cb92d7d68839f4232904f306",
+    "url": "/FCC-random-quote-machine/static/media/background-mobile.b0c85541.svg"
+  },
+  {
+    "revision": "87fdbe4779a09d178853992a03ccbdcc",
+    "url": "/FCC-random-quote-machine/static/media/twitter-black-shape.87fdbe47.svg"
+  }
+]);
